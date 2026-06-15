98c59f70d4f2eb475b047cb3ff9d26aa14b11a64
azacarias-dev <azacarias-2024007@kinal.edu.gt>
2026-06-15 11:42:43 -0600

    fix: agregar url del frontend a los cors

--- Administrador/configs/cors-configuration.js
+++ Administrador/configs/cors-configuration.js
@@ -1,5 +1,5 @@
 export const corsOptions = {
-    origin: 'http://localhost:5173', 
+    origin: 'http://localhost:5174', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], 
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
